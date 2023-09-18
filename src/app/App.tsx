b4d788e92bb45c58ca2ab94a9718d0dd4213cebf
Jeanings Ng <jeanings.ng@gmail.com>
2023-09-18 11:19:18 -0700

    Add dimming overlay for editing mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAppSelector } from './hooks';
 import Todo from '../features/Todo/Todo';
 import LegendSelector from '../features/LegendSelector/LegendSelector';
 import TodoCreator from '../features/TodoCreator/TodoCreator';
@@ -10,8 +11,15 @@ import './App.css';
 export const todoApiUri = process.env.REACT_APP_BACKEND_URI as string;
 
 function App() {
+    const editing = useAppSelector(state => state.editor.editing);
+
     return (
         <div className="App">
+            {/* Dimming overlay when Editor is shown. */}
+            <div className={ `App__overlay${editing ? " on" : " off"}` }
+                role="figure"
+                aria-label="overlay for main app">
+            </div>
             <Todo />
             <LegendSelector />
             <TodoCreator />
